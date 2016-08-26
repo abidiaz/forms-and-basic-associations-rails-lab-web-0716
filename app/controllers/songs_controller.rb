15929e5870d74ca968a3dcff7ba6f6f1572df013
Abigail Diaz <github email address>
2016-08-26 10:47:42 -0400

    Completed forms and basic associations lab

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,12 @@
 class SongsController < ApplicationController
+  before_action :song_params, only: [:create, :update]
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
   def index
     @songs = Song.all
   end
 
   def show
-    @song = Song.find(params[:id])
   end
 
   def new
@@ -13,32 +15,26 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-
     if @song.save
-      redirect_to @song
+      redirect_to song_path(@song)
     else
-      render :new
+      render 'new'
     end
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
-
     @song.update(song_params)
-
     if @song.save
-      redirect_to @song
+      redirect_to song_path(@song)
     else
-      render :edit
+      render 'edit'
     end
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
@@ -47,7 +43,11 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents=>[])
   end
+
+  def set_song
+    @song = Song.find(params[:id])
+  end 
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,43 @@
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  # attr_reader :artist_name, :genre_name
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+   def genre_name
+    self.genre.name if self.genre
+  end
+
+  def note_contents=(note_contents)
+    note_contents.each do |content|
+      if content != ''
+        self.notes << Note.create(content: content)    
+      end
+    end
+  end
+
+   def note_contents
+    array = []
+    self.notes.each do |note|
+      array << note.content
+    end
+    array
+  end
+
 end
 
